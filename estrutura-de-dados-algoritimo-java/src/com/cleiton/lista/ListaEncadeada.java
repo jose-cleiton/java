994553a4387508a76617f1e54f7dbdd3fb4438a3
jose-cleiton <cleitons835@gmail.com>
2022-12-19 21:39:33 -0300

    Listas encadeadas java

---
+++ estrutura-de-dados-algoritimo-java/src/com/cleiton/lista/ListaEncadeada.java
@@ -0,0 +1,6 @@
+
+public class ListaEncadeada {
+
+  private No inicio;
+  
+}
---
+++ estrutura-de-dados-algoritimo-java/src/com/cleiton/lista/No.java
@@ -0,0 +1,25 @@
+
+public class No<T> {
+
+  private T elemento;
+  private No<T> proximo;
+  
+  
+  public T getElemento() {
+    return elemento;
+  }
+  
+  public void setElemento(T elemento) {
+    this.elemento = elemento;
+  }
+
+  public No<T> getProximo() {
+    return proximo;
+  }
+  public void setProximo(No<T> proximo) {
+    this.proximo = proximo;
+  }
+
+ 
+  
+}
