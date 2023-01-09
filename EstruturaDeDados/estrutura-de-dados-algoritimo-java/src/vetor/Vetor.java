34c778225b67a44dfb12b7fc9c8750e5c359a42d
jose-cleiton <cleitons835@gmail.com>
2023-01-09 19:01:25 -0300

    feat: :sparkles: iniciando metodo adicionar

--- EstruturaDeDados/estrutura-de-dados-algoritimo-java/src/vetor/Vetor.java
+++ EstruturaDeDados/estrutura-de-dados-algoritimo-java/src/vetor/Vetor.java
@@ -7,5 +7,14 @@ public class Vetor {
   public Vetor(int capacidade) {
     this.elementos = new String[capacidade];
   }
+
+  public void adiciona(String elemento) {
+    for (int i = 0; i < this.elementos.length; i++) {
+      if (this.elementos[i] == null) {
+          this.elementos[i] = elemento;
+          break;
+      }
+    }
+  }
   
 }
--- EstruturaDeDados/estrutura-de-dados-algoritimo-java/src/vetor_teste/Aula02.java
+++ EstruturaDeDados/estrutura-de-dados-algoritimo-java/src/vetor_teste/Aula02.java
@@ -6,7 +6,6 @@ public class Aula02 {
   
   public static void main(String[] args) {
     Vetor vetor = new Vetor(5);
-    System.out.println(vetor);
-  }
+    vetor.adiciona("elemento 1");  }
   
 }
