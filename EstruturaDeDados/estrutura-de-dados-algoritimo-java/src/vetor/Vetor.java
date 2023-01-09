3fdf436a460c572a9a312df06ab22f5b7dc2374b
jose-cleiton <cleitons835@gmail.com>
2023-01-09 18:47:20 -0300

    feat: :sparkles: iniciando estrutura de dados java

---
+++ EstruturaDeDados/estrutura-de-dados-algoritimo-java/src/vetor/Vetor.java
@@ -0,0 +1,11 @@
+package vetor;
+
+public class Vetor {
+
+  private String[] elementos;
+
+  public Vetor(int capacidade) {
+    this.elementos = new String[capacidade];
+  }
+  
+}
---
+++ EstruturaDeDados/estrutura-de-dados-algoritimo-java/src/vetor_teste/Aula02.java
@@ -0,0 +1,12 @@
+package vetor_teste;
+
+import vetor.Vetor;
+
+public class Aula02 {
+  
+  public static void main(String[] args) {
+    Vetor vetor = new Vetor(5);
+    System.out.println(vetor);
+  }
+  
+}
