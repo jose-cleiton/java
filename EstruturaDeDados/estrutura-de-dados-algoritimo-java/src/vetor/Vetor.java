6751583677e72c879beb6c7361dc32809b839411
jose-cleiton <cleitons835@gmail.com>
2023-01-09 19:20:11 -0300

    feat: :sparkles: Adicionar elemento ao final do vetor

--- EstruturaDeDados/estrutura-de-dados-algoritimo-java/src/vetor/Vetor.java
+++ EstruturaDeDados/estrutura-de-dados-algoritimo-java/src/vetor/Vetor.java
@@ -1,20 +1,25 @@
 package vetor;
 
+import java.lang.Exception;
+
 public class Vetor {
 
   private String[] elementos;
+  private int size;
 
   public Vetor(int capacidade) {
     this.elementos = new String[capacidade];
+    this.size = 0;
   }
 
-  public void adiciona(String elemento) {
-    for (int i = 0; i < this.elementos.length; i++) {
-      if (this.elementos[i] == null) {
-          this.elementos[i] = elemento;
-          break;
-      }
+  public void adiciona(String elemento) throws Exception {
+    if (this.size < this.elementos.length) {
+      this.elementos[size] = elemento;
+      this.size++;
+    }
+    else {
+      throw new Exception("Is already full");
     }
   }
-  
+
 }
--- EstruturaDeDados/estrutura-de-dados-algoritimo-java/src/vetor_teste/Aula02.java
+++ EstruturaDeDados/estrutura-de-dados-algoritimo-java/src/vetor_teste/Aula02.java
@@ -4,8 +4,15 @@ import vetor.Vetor;
 
 public class Aula02 {
   
-  public static void main(String[] args) {
-    Vetor vetor = new Vetor(5);
-    vetor.adiciona("elemento 1");  }
+  public static void main(String[] args) throws Exception {
+    Vetor vetor = new Vetor(3);
+    try {
+      vetor.adiciona("elemento 1");
+      vetor.adiciona("elemento 2");
+      vetor.adiciona("elemento 3");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }  
+      }
   
 }
