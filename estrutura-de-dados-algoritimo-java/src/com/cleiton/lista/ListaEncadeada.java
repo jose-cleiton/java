23d0df80f9fe19fd4a8b766c4472f6425926988b
jose-cleiton <cleitons835@gmail.com>
2023-01-09 12:33:19 -0300

    feat: :sparkles: implementação da classe vetor
    Esse é um código de uma classe chamada Vetor, que representa um vetor de elementos do tipo String. Ela possui diversos métodos, como adiciona, que adiciona um elemento ao vetor, remove, que remove um elemento do vetor, busca, que busca por um elemento ou sua posição no vetor, tamanho, que retorna o tamanho do vetor, e toString, que retorna uma representação do vetor como uma string.  É importante notar que alguns dos métodos estão implementados de maneira incompleta, ou seja, eles possuem uma implementação básica, mas ainda precisam ser desenvolvidos para realizar suas funções de maneira correta. Por exemplo, o método adiciona não está adicionando o elemento ao vetor de fato, e o método busca sempre retorna -1, independentemente do elemento passado como parâmetro.

--- estrutura-de-dados-algoritimo-java/src/com/cleiton/lista/ListaEncadeada.java
+++
@@ -1,6 +0,0 @@
-
-public class ListaEncadeada {
-
-  private No inicio;
-  
-}
---
+++ estrutura-de-dados-algoritimo-java/vetor/Vetor.java
@@ -0,0 +1,41 @@
+
+
+public class Vetor {
+
+  private String[] elementos;
+
+  public Vetor(int capacidade) {
+    this.elementos = new String[capacidade];
+  }
+
+  public void adiciona(String elemento) {
+
+  }
+
+  public void adiciona(int posicao, String elemento) {
+
+  }
+
+  public void remove(int posicao) {
+
+  }
+
+  public String busca(int posicao) {
+    return null;
+  }
+
+  public int busca(String elemento) {
+    return -1;
+  }
+
+  public int tamanho() {
+    return 0;
+  }
+
+  public String toString() {
+    return null;
+  }
+
+
+}//fim
+
